d941b6b4dd476f5efdb2351e66341e230e18f510
fansiphone <1024007233@qq.com>
2025-08-20 14:56:25 +0800

    Update notify.py

--- api/notify.py
+++ api/notify.py
@@ -1,76 +1,44 @@
 from http.server import BaseHTTPRequestHandler
 import json
 import apprise
-from io import BytesIO
+ 
+class handler(BaseHTTPRequestHandler):
 
-def handler(request):
-    # 将 Vercel 的请求转换为 BaseHTTPRequestHandler 兼容格式
-    class RequestHandler(BaseHTTPRequestHandler):
-        def __init__(self):
-            self.request = request
-            self.wfile = BytesIO()
-        
-        def do_GET(self):
+    def do_GET(self):
+        self.send_response(400)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Apprise Vercel is working, use POST method to send notifications.'.encode('utf-8'))
+ 
+    def do_POST(self):
+        if 'content-type' not in self.headers or self.headers['content-type'] != 'application/json':
             self.send_response(400)
-            self.send_header('Content-type', 'text/plain')
+            self.send_header('Content-type','text/plain')
             self.end_headers()
-            self.wfile.write(b'Apprise Vercel is working, use POST method to send notifications.')
-            return self.wfile.getvalue()
+            self.wfile.write('Currently `content-type` must be `application/json` (which is different from caronc/apprise-api)'.encode('utf-8'))
+            return
+
+        # Currently we only support JSON
+        length = int(self.headers['content-length'])
+        form = json.loads(self.rfile.read(length))
         
-        def do_POST(self):
-            if self.request.headers.get('content-type') != 'application/json':
-                self.send_response(400)
-                self.send_header('Content-type', 'text/plain')
-                self.end_headers()
-                self.wfile.write(b'Content-Type must be application/json')
-                return self.wfile.getvalue()
-            
-            try:
-                data = json.loads(self.request.body)
-                apobj = apprise.Apprise()
-                
-                for url in data['urls'].split(","):
-                    apobj.add(url)
-                
-                if apobj.notify(
-                    body=data['body'],
-                    title=data.get('title', ''),
-                    notify_type=data.get('type', 'info'),
-                    body_format=data.get('format', 'text'),
-                ):
-                    self.send_response(200)
-                    self.send_header('Content-type', 'text/plain')
-                    self.end_headers()
-                    self.wfile.write(b'OK')
-                else:
-                    self.send_response(500)
-                    self.send_header('Content-type', 'text/plain')
-                    self.end_headers()
-                    self.wfile.write(b'Error sending notifications')
-                
-                return self.wfile.getvalue()
-            
-            except Exception as e:
-                self.send_response(500)
-                self.send_header('Content-type', 'text/plain')
-                self.end_headers()
-                self.wfile.write(f'Error: {str(e)}'.encode())
-                return self.wfile.getvalue()
-    
-    # 根据请求方法调用对应的处理函数
-    handler = RequestHandler()
-    if request.method == 'GET':
-        return {
-            'statusCode': 400,
-            'body': handler.do_GET()
-        }
-    elif request.method == 'POST':
-        return {
-            'statusCode': 200,
-            'body': handler.do_POST()
-        }
-    else:
-        return {
-            'statusCode': 405,
-            'body': 'Method Not Allowed'
-        }
+        apobj = apprise.Apprise()
+
+        for url in form['urls'].split(","):
+            apobj.add(url)
+        
+        if apobj.notify(
+            body=form['body'],
+            title=form.get('title', ''),
+            notify_type=form.get('type', 'info'),
+            body_format=form.get('format', 'text'),
+        ):
+            self.send_response(200)
+            self.send_header('Content-type','text/plain')
+            self.end_headers()
+            self.wfile.write('OK'.encode('utf-8'))
+        else:
+            self.send_response(500)
+            self.send_header('Content-type','text/plain')
+            self.end_headers()
+            self.wfile.write('Error sending notifications'.encode('utf-8'))
